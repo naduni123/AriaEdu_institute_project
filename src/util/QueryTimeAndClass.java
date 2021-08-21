14cf3ccb347f996c1b730b254d6357836824df87
iamKAviB <iamkavib@gmail.com>
2021-08-21 09:49:09 +0530

    backend for loading items to jsp

--- src/util/QueryTimeAndClass.java
+++ src/util/QueryTimeAndClass.java
@@ -7,5 +7,12 @@ public class QueryTimeAndClass {
     public static final String SEE_CLASS="SELECT * FROM classroom";
 
 
+    public static final String ADD_TIMESLOT = "INSERT INTO timeslot(batch,day,starttime,endtime,subject,teacher,classroom) values(?,?,?,?,?,?,?)";
+
+
+
+    public static final String SEE_BATCH= "SELECT * FROM batch";
+    public static final String FIND_TEACHER="SELECT * FROM teacher WHERE id = ";
+    public static final String SEE_TEACHERS="SELECT * FROM teacher";
 
 }
