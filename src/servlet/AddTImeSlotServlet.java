0317869d3b5d93ec788e3438c0d740c89266618c
iamKAviB <iamkavib@gmail.com>
2021-08-26 22:24:37 +0530

    add time slot servlet bug fix

--- src/servlet/AddTImeSlotServlet.java
+++ src/servlet/AddTImeSlotServlet.java
@@ -18,19 +18,36 @@ public class AddTImeSlotServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         TimeSlot timeSlot = new TimeSlot();
+        TimeSlotService timeSlotService = new TimeSlotService();
 
         timeSlot.setBatch(Integer.parseInt(request.getParameter("batch")));
         timeSlot.setDate(request.getParameter("day"));
-        System.out.println(request.getParameter("starttime"));
-        timeSlot.setStartTime(request.getParameter("starttime"));
-        timeSlot.setEndTime(request.getParameter("endtime"));
-        timeSlot.setSubject(request.getParameter("subject"));
-        timeSlot.setTeacher(Integer.parseInt(request.getParameter("teacher")));
-        timeSlot.setClassroom(Integer.parseInt(request.getParameter("classroom")));
 
-        TimeSlotService timeSlotService = new TimeSlotService();
-        timeSlotService.AddTimeSlot(timeSlot);
-        response.sendRedirect("indexhome.jsp");
+
+        String start = request.getParameter("starttime");
+        String end = request.getParameter("endtime");
+        String classroom = request.getParameter("classroom");
+
+        boolean check = timeSlotService.check(start,end,classroom);
+        System.out.println(check);
+
+        if(check == false) {
+
+            timeSlot.setBatch(Integer.parseInt(request.getParameter("batch")));
+            timeSlot.setDate(request.getParameter("day"));
+            timeSlot.setStartTime(request.getParameter("starttime"));
+            timeSlot.setEndTime(request.getParameter("endtime"));
+            timeSlot.setSubject(request.getParameter("subject"));
+            timeSlot.setTeacher(Integer.parseInt(request.getParameter("teacher")));
+            timeSlot.setClassroom(Integer.parseInt(request.getParameter("classroom")));
+
+
+            timeSlotService.AddTimeSlot(timeSlot);
+            response.sendRedirect("timetabledashboard.jsp");
+
+        }else{
+            response.sendRedirect("add_time_slot.jsp");
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
