a632be63117c4e34b29afb9e5e205ff0bc9d4aca
iamKAviB <iamkavib@gmail.com>
2021-08-26 13:26:36 +0530

    js

---
+++ web/assets/libs/js/timeslot.js
@@ -0,0 +1,131 @@
+function validateform(){
+
+
+
+
+}
+
+function batchvalidate(){
+
+    let batch = document.forms['form']['batch'].value;
+
+    if(batch=="0"){
+
+        document.getElementById('batchcheck').innerHTML="please select a batch";
+        return false;
+
+    }else{
+        return  true;
+    }
+
+}
+
+function dayvalidate(){
+
+    let day= document.forms['form']['day'].value;
+
+    if(day=="0"){
+
+        document.getElementById('daycheck').innerHTML="please select a day";
+        return false;
+
+    }else{
+        return  true;
+    }
+
+}
+
+function startvalidate(){
+
+    let start= document.forms['form']['starttime'].value;
+
+    if(start==""){
+
+        document.getElementById('starttimecheck').innerHTML="please enter start time";
+        return false;
+
+    }else{
+        return  true;
+    }
+
+}
+
+function endvalidate(){
+
+    let end= document.forms['form']['endtime'].value;
+    let start= document.forms['form']['starttime'].value;
+
+    if(end==""){
+
+        document.getElementById('starttimecheck').innerHTML="please enter end time";
+        return false;
+
+    }else if(end< start){
+        document.getElementById('starttimecheck').innerHTML=" end time should greater than start time";
+        return  false;
+    }else{
+
+        return true;
+    }
+
+}
+
+function subjectvalidate(){
+
+    let subject= document.forms['form']['subject'].value;
+
+    if(subject=="0"){
+
+        document.getElementById('endcheck').innerHTML="please enter subject";
+        return false;
+
+    }else{
+        return  true;
+    }
+
+}
+
+function subjectvalidate(){
+
+    let subject= document.forms['form']['subject'].value;
+
+    if(subject=="0"){
+
+        document.getElementById('subjectcheck').innerHTML="please select a subject";
+        return false;
+
+    }else{
+        return  true;
+    }
+
+}
+
+function teachervalidate(){
+
+    let teacher= document.forms['form']['teacher'].value;
+
+    if(teacher=="0"){
+
+        document.getElementById('subjectcheck').innerHTML="please select a teacher";
+        return false;
+
+    }else{
+        return  true;
+    }
+
+}
+
+function classvalidate(){
+
+    let teacher= document.forms['form']['classroom'].value;
+
+    if(classroom=="0"){
+
+        document.getElementById('classrromcheck').innerHTML="please select a class";
+        return false;
+
+    }else{
+        return  true;
+    }
+
+}
