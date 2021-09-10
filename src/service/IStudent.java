2860e1ef5495ebe8c9db02a68b5dd3e07b769ddd
HansakaDilshanJayawardana <hansaka345@gmail.com>
2021-09-10 21:16:33 +0530

    Istudent service latest fix

--- src/service/IStudent.java
+++ src/service/IStudent.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public interface IStudent {
 
-    public void addStudent(Student student);
+    public void addStudent(Student student);//addStudent
     public void editStudent(Student student);
     public ArrayList<Student> ViewStudent();
     public Student selectStudent(int id);
