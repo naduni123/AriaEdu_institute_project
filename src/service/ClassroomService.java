e67895e6f964e99aa2c8e9b62fe6678c4f6df942
iamKAviB <iamkavib@gmail.com>
2021-09-28 08:23:53 +0530

    service

--- src/service/ClassroomService.java
+++ src/service/ClassroomService.java
@@ -43,7 +43,28 @@ public class ClassroomService implements IClassroom {
 
     @Override
     public void editClassroom(Classroom classroom) {
+        try {
+            con = DBConnectionUtil.getConnection();
+            String sql = QueryTimeAndClass.UPDATE_CLASS+classroom.getId();
+            preparedStatement = con.prepareStatement(sql);
+            System.out.println(sql);
 
+
+            preparedStatement.setString(1,classroom.getName());
+            preparedStatement.setString(2,classroom.getFloor());
+            preparedStatement.setInt(3,classroom.getCapacity());
+            preparedStatement.setString(4,classroom.getAc());
+
+
+
+            System.out.println(sql);
+
+            preparedStatement.execute();
+
+        }catch(Exception e){
+
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -86,6 +107,43 @@ public class ClassroomService implements IClassroom {
         }
         return list;
 
+
+
+    }
+
+
+    public Classroom findClassroom(int id){
+
+        Classroom classroom =  new Classroom();
+
+        try {
+
+            con = DBConnectionUtil.getConnection();
+            String sql=QueryTimeAndClass.FIND_CLASS+id;
+
+            preparedStatement = con.prepareStatement(sql);
+            System.out.println(preparedStatement);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while(rs.next()) {
+
+                classroom.setId(rs.getInt(1));
+                classroom.setName(rs.getString(2));
+                classroom.setFloor(rs.getString(3));
+                classroom.setCapacity(rs.getInt(4));
+                classroom.setAc(rs.getString(5));
+
+
+                System.out.println("service" + classroom.getAc());
+
+            } 
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return classroom;
 
 
     }
