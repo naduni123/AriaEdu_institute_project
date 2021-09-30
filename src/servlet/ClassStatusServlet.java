9e9bc454a7e2e6be4bf3e80a3e4f7f55d93c686d
iamKAviB <iamkavib@gmail.com>
2021-09-30 12:00:14 +0530

    new files

---
+++ src/servlet/ClassStatusServlet.java
@@ -0,0 +1,35 @@
+package servlet;
+
+import model.Classroom;
+import service.ClassroomService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/ClassStatusServlet")
+public class ClassStatusServlet extends HttpServlet {
+
+    ClassroomService classroomService= new ClassroomService();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Classroom> list = new ArrayList<>();
+        try{
+            list = classroomService.viewClassroom();
+
+            request.setAttribute("list",list);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/servlet/DeleteTeacherTimeServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import service.TimeSlotService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet( "/DeleteTeacherTimeServlet")
+public class DeleteTeacherTimeServlet extends HttpServlet {
+    TimeSlotService timeSlotService = new TimeSlotService();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        timeSlotService.deleteTime(id);
+
+        request.getRequestDispatcher("teachertimetable.jsp").forward(request,response);
+    }
+}
